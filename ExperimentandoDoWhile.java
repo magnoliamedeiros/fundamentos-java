4b012b3dfcf4f607f42253695b4e42a4f6c2e97a
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-13 21:23:53 -0300

    Exemplo simples que mostra a utilizacao do DO WHILE

--- ExperimentandoDoWhile.java
+++ ExperimentandoDoWhile.java
@@ -1,11 +1,17 @@
 public class ExperimentandoDoWhile {
     public static void main (String[] args) {
-        int numero = 100;
+
+        int numero = 0;
 
         // Executa pelo menos 1 vez
         do {
             System.out.println("O valor do numero = " + numero);
-            numero++;
+
+            // modificando a variável que faz parte da condição
+            numero = numero + 1;
         } while (numero <= 10);
+
+        System.out.println("Fim do codigo...");
+
     }
 }
