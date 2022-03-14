3ea14ef19fc20553a1390cdb1c53a5afc6511ae2
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-13 22:01:30 -0300

    Exemplo simples que mostra a utilizacao do FOR

---
+++ ExperimentandoFor.java
@@ -0,0 +1,11 @@
+public class ExperimentandoFor {
+    public static void main (String[] args) {
+
+        int numero;
+
+        for (numero = 0; numero <=10; numero++) {
+            System.out.println("Valor do numero = " + numero);
+        }
+        System.out.println("Fim do codigo...");
+    }
+}
