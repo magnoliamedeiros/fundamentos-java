a83dbd0023762249640a7babe79583d35b633d79
maguidev <magnoliamdeiros@hotmail.com>
2022-06-09 13:37:04 -0300

    correcao de erros

--- TabuadaDeUmaLinha.java
+++ TabuadaDeUmaLinha.java
@@ -1,6 +1,5 @@
 public class TabuadaDeUmaLinha {
     public static void main(String[] args) {
-        for (int numero = (new java.util.Scanner(System.in)).nextInt(), cont=1; cont <= 10;
-        System.out.println(numero + " x " + cont + " = " + numero * cont++));
+        for (int numero = (new java.util.Scanner(System.in)).nextInt(), cont=1; cont <= 10; System.out.println(numero + " x " + cont + " = " + numero * cont++));
     }
 }
